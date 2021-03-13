763a0f6c82abaa59d4ab5f35ee7111a13e798613
Эрик <erik.petrosyan.2222@mail.ru>
2021-03-13 09:29:37 +0500

    init

---
+++ Labarator1/Labarator1/Base.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labarator1
+{
+    public partial class Base : Form
+    {
+        public Base()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Entrance newForm = new Entrance(this);
+            newForm.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Registration newForm = new Registration(this);
+            newForm.Show();
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Base_Load(object sender, EventArgs e)
+        {
+            userBindingSource.Add(new User() {Login = "admin", 
+                                              Email = "admin@mail.ru",
+                                              Name = "admin",
+                                              Surname = "admin",
+                                              Telephone = "+7(999)999-99-99",
+                                             });
+        }
+
+        private void userBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bindingNavigatorDeleteItem.PerformClick();
+        }
+    }
+}
---
+++ Labarator1/Labarator1/Registration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labarator1
+{
+
+    public partial class Registration : Form
+    {
+        Base Base = new Base();
+        public Registration()
+        {
+            InitializeComponent();
+        }
+        public Registration(Base Base2)
+        {
+            InitializeComponent();
+            Base = Base2;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (User user1 in Base.userBindingSource)
+            {
+                if (user1.Email == textBox1.Text || user1.Login == textBox2.Text)
+                { MessageBox.Show("Такой пользователь или почта существует!"); return; }
+            }
+
+            User user = new User() { Login = textBox1.Text, Password = textBox2.Text, Name = textBox3.Text, Surname = textBox4.Text, Telephone = textBox5.Text};
+            Base.userBindingSource.Add(user);
+
+
+        }
+    }
+}
